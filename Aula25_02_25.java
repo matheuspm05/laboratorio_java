66508c00f7439d8ffa90e9afc2b0355ac0317ebe
matheuspm05 <159352318+matheuspm05@users.noreply.github.com>
2025-02-26 19:43:43 -0300

    Update Aula25_02_25.java

--- Aula25_02_25.java
+++ Aula25_02_25.java
@@ -1,8 +1,11 @@
-package com.mycompany.aula25_02_25;
-
 import java.util.Scanner;
 
 public class Aula25_02_25 {
+
+    public static void valorConta(Conta c1) {
+        System.out.println("------------------------------------------");
+        System.out.println("VALOR DA CONTA: " + c1.saldo);
+    }
 
     public static void main(String[] args) {
         int valor, opcao = -1;
@@ -14,28 +17,48 @@ public class Aula25_02_25 {
         System.out.println("CRIAR UMA CONTA");
         System.out.println("------------------------------------------");
 
-        System.out.println("DIGITE O NUMERO DA CONTA:");
+        System.out.print("DIGITE O NÚMERO DA CONTA: ");
         c1.numero = scan.nextInt();
         scan.nextLine();
-        
-        System.out.println("DIGITE SEU NOME:");
+
+        System.out.print("DIGITE SEU NOME: ");
         c1.nome = scan.nextLine();
 
         do {
-            System.out.println("DIGITE 1 (SACAR)");
-            System.out.println("DIGITE 2 (DEPOSITAR)");
+            System.out.println("------------------------------------------");
+            System.out.println("DIGITE 1 (CONSULTAR SALDO)");
+            System.out.println("DIGITE 2 (SACAR)");
+            System.out.println("DIGITE 3 (DEPOSITAR)");
             System.out.println("DIGITE 0 (SAIR)");
+            System.out.println("------------------------------------------");
             opcao = scan.nextInt();
-            if(opcao == 1){
-                System.out.println("DIGITE O VALOR QUE QUEIRA SACAR:)");
+
+            if (opcao == 1) {
+                valorConta(c1);
+            } else if (opcao == 2) {
+                System.out.print("DIGITE O VALOR QUE QUEIRA SACAR: ");
                 valor = scan.nextInt();
-                
+                c1.sacar(valor);
+
+                valorConta(c1);
+            } else if (opcao == 3) {
+                System.out.print("DIGITE O VALOR QUE QUEIRA DEPOSITAR: ");
+                valor = scan.nextInt();
+                c1.depositar(valor);
+                valorConta(c1);
+            } else {
+                System.out.println("VOCE ESCOLHEU SAIR!");
             }
-            
+
         } while (opcao != 0);
-        
-       scan.close();
+
+        System.out.println("------------------------------------------");
+        System.out.println("NUMERO CONTA: " + c1.numero);
+        System.out.println("SEU NOME: " + c1.nome);
+        System.out.println("LIMITE DISPONÍVEL: " + c1.limite);
+        System.out.println("SALDO FINAL: " + c1.saldo);
+        System.out.println("------------------------------------------");
+
+        scan.close();
     }
-
 }
-
