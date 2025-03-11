9dbd2903f4b9992fd7669ac7c42aa05a54327373
matheuspm05 <159352318+matheuspm05@users.noreply.github.com>
2025-03-11 10:53:11 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,11 @@
+ public class Cliente{
+        String nome;
+        String sobrenome;
+        String cpf;
+        Cliente conjunge;
+        
+        public void casar(Cliente conjunge){
+            
+            
+        }
+    }
