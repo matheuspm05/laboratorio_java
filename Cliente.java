449ebe8768d26b92c2f6a0453169e2bf4cfe5081
matheuspm05 <159352318+matheuspm05@users.noreply.github.com>
2025-03-11 23:42:16 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,11 +1,16 @@
- public class Cliente{
-        String nome;
-        String sobrenome;
-        String cpf;
-        Cliente conjunge;
-        
-        public void casar(Cliente conjunge){
-            
-            
+public class Cliente{
+    String nome;
+    String sobrenome;
+    String cpf;
+    Cliente conjuge;
+    
+    public void casar(Cliente conjuge) {
+        if (this.conjuge != null) {
+            this.conjuge = conjuge;
+            this.conjuge.conjuge = this;
+        } else {
+            System.out.println("ja casado");
         }
+        
     }
+}
