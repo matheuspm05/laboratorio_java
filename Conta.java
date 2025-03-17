36c62e2ce76834a930fd69ccd43c0b5befa147c9
matheuspm05 <159352318+matheuspm05@users.noreply.github.com>
2025-03-16 21:30:27 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -2,25 +2,41 @@ public class Conta {
 
     int numero;
     double limite = 500;
-    double saldo = 1000;
+    double saldo;
     Cliente titular;
 
     Conta() {
         this.titular = new Cliente();
+        limite = 500;
+        saldo = 1000;
     }
-    
-   public void sacar(double v) {
-        if (saldo >= v) {
-            saldo -= v;
-            System.out.println("SAQUE DE R$" + v + " REALIZADO COM SUCESSO.");
+
+    public void sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            System.out.println("SAQUE DE R$" + valor + " REALIZADO COM SUCESSO.");
+        } else if (valor <= saldo + limite) {
+            double restante = valor - saldo;
+            saldo = 0;
+            limite -= restante;
+            System.out.println("SAQUE DE R$" + valor + " REALIZADO COM SUCESSO USANDO O LIMITE.");
         } else {
             System.out.println("ERRO: SALDO INSUFICIENTE! VOCÊ TEM APENAS R$" + saldo);
         }
     }
 
-    public void depositar(double v) {
-        saldo += v;
-        System.out.println("DEPÓSITO DE R$" + v + " REALIZADO COM SUCESSO.");
+    public void valorLim() {
+        System.out.println("VALOR LIMITE: " + limite);
     }
-   
+
+    public void depositar(double valor) {
+        if (valor >= 0) {
+        saldo += valor;
+        System.out.println("DEPÓSITO DE R$" + valor + " REALIZADO COM SUCESSO.");
+        }else {
+            System.out.println("VALOR INVALIDO");
+        }
+       
+    }
+
 }
