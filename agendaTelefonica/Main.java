336b3b7d7bbb1a8aa6b52719c81506d54a002ac3
matheuspm05 <159352318+matheuspm05@users.noreply.github.com>
2025-04-14 21:07:07 -0300

    Update Main.java

--- agendaTelefonica/Main.java
+++ agendaTelefonica/Main.java
@@ -1,21 +1,33 @@
-
 public class Main {
 
     public static void main(String[] args) {
 
-        AgendaTelefonica agenda1 = new AgendaTelefonica();
+        AgdTelefonicaNormal agenda1 = new AgdTelefonicaNormal();
+        AgdTelefonicaObj agenda2 = new AgdTelefonicaObj();
 
+        // agenda 1
         agenda1.inserir("matheus", "289999332");
         agenda1.inserir("João", "289998441");
         agenda1.inserir("Davi", "289992222");
-        
+
         System.out.println("Número de Matheus: " + agenda1.buscarNumero("matheus"));
 
         agenda1.remover("João");
 
-        
         System.out.println("Tamanho da agenda: " + agenda1.tamanho());
 
-        //agenda1.printar();
+        agenda1.printar();
+
+        // agenda 2
+        agenda2.inserir("simo", new Pessoa("gabriel", 22222));
+        agenda2.inserir("nigel", new Pessoa("simonaci", 242233));
+        agenda2.inserir("cratiel", new Pessoa("volpini", 2734422));
+
+        Pessoa p = agenda2.buscarPessoa("nigel");
+        System.out.println("\nPessoa encontrada: " + p);
+
+        
+        System.out.println("\nContatos completos:");
+        agenda2.printarObj();
     }
 }
