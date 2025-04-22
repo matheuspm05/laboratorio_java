7b65428d4098873b4a077b1673cdd59c7105f40c
matheuspm05 <159352318+matheuspm05@users.noreply.github.com>
2025-04-22 10:36:41 -0300

    Update Evento.java

--- Geren Participantes/Evento.java
+++ Geren Participantes/Evento.java
@@ -1,68 +1,66 @@
 import java.util.Set;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Iterator;
 
 public class Evento {
 
-    private Set<Participante> participantes;
-    private String nomeEvento;
+	private Set<Participante> participantes;
+	private String nomeEvento;
 
-    public Evento(String nome) {
-        this.nomeEvento = nome;
-
+	public Evento(String nome) {
+		this.nomeEvento = nome;
         participantes = new HashSet<>();
+	}
 
-    }
+	public String getNome() {
+		return nomeEvento;
+	}
 
-    public String getNome() {
-        return nomeEvento;
-    }
-
-    public void AddParticipantes(Participante participante) {
-        if(verificarParticipante(participante.getEmail()) == true){
-            participantes.add(participante);
-        }
-    }
+	public void AddParticipantes(Participante participante) {
+		if(verificarParticipante(participante.getEmail()) == true) {
+			participantes.add(participante);
+		}
+	}
 
     public void adicionarTodas(List<Participante> novosParticipantes) {
-        if(verificarParticipante(List<Participante>.getEmail()) == true){
-            participantes.addAll(List<Participante>);
-        }
-    }
-
-    private boolean verificarParticipante(String email) {
-        for (Participante ptc : participantes) {
-            if (ptc.getEmail().equals(email)) {
-                System.out.println("JA EXISTE O PARTICIPANTE");
-                return false;
+        for (Participante participante : novosParticipantes) {
+            if (verificarParticipante(participante.getEmail())) {
+                participantes.add(participante);
             }
         }
-        return true;
     }
 
-    public void removerParticipante(String email) {
-        
-        Iterator<Participante> iterator = participantes.iterator();
-        
-        while (iterator.hasNext()) {
-            Participante ptc = iterator.next();
-            if (ptc.getEmail().equals(email)) {
-                iterator.remove();  
-                System.out.println(ptc.getNome() + " REMOVIDO COM SUCESSO!");
-                break; 
-            }
-        }
-        System.out.println("Participante com o email " + email + " não encontrado.");
-    }
+	private boolean verificarParticipante(String email) {
+		for (Participante ptc : participantes) {
+			if (ptc.getEmail().equals(email)) {
+				System.out.println("JA EXISTE O PARTICIPANTE\n");
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public void imprimirParticipantes() {
-        for (Participante ptc : participantes) {
-            if (ptc != null) {
-                 System.out.println(ptc);
-            }
-           
-        }
+	public void removerParticipante(String email) {
 
-    }
+		Iterator<Participante> iterator = participantes.iterator();
+
+		while (iterator.hasNext()) {
+			Participante ptc = iterator.next();
+			if (ptc.getEmail().equals(email)) {
+				iterator.remove();
+				System.out.println(ptc.getNome() + " REMOVIDO COM SUCESSO!");
+				return;
+			}
+		}
+		System.out.println("Participante com o email " + email + " nao encontrado.");
+	}
+
+	public void imprimirParticipantes() {
+		for (Participante ptc : participantes) {
+			System.out.println(ptc);
+		}
+
+	}
 
 }
