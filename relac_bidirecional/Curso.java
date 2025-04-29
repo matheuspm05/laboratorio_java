a849bcac298b801e53baf361785dc45ee9f1d1d7
matheuspm05 <159352318+matheuspm05@users.noreply.github.com>
2025-04-29 10:23:26 -0300

    Update Curso.java

--- relac_bidirecional/Curso.java
+++ relac_bidirecional/Curso.java
@@ -2,35 +2,40 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Curso {
-    private String nome;
-    private List<Estudante> estudantes;
+	private String nome;
+	private List<Estudante> estudantes;
 
-    public Curso(String nome) {
-        this.nome = nome;
-        estudantes = new ArrayList<>();
-    }
+	public Curso(String nome) {
+		this.nome = nome;
+		estudantes = new ArrayList<>();
+	}
 
-    public void addEstudante(Estudante estudante) {
-        if (!estudantes.contains(estudante)) { 
-            estudantes.add(estudante);
-            estudante.inscreverEmCurso(this);
-        }
-        
-    }
+	public void addEstudante(Estudante estudante) {
+		if (!estudantes.contains(estudante)) {
+			estudantes.add(estudante);
+			estudante.inscreverEmCurso(this);
+		}
 
-    public String getNome() {
-        return nome;
-    }
-    
+	}
 
-    public void imprimirEstudantes() {
-        for (Estudante estudante : estudantes) {
-            System.out.println(estudante);
-        }
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    @Override
-    public String toString() {
-        return "curso: " + nome;
-    }
+
+	public void imprimirEstudantes() {
+		System.out.println("+--------------------------------------------+");
+		System.out.println("| Curso: " + nome);
+		System.out.println("| Nome dos estudantes \n" + "|");
+		for (Estudante estudante : estudantes) {
+			System.out.println("| - " + estudante);
+		}
+		System.out.println("+--------------------------------------------+");
+	}
+
+
+	@Override
+	public String toString() {
+		return "Curso: " + nome;
+	}
 }
