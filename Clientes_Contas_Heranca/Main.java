0025f95037ac6284fd08919d71a921b0006bc424
matheuspm05 <159352318+matheuspm05@users.noreply.github.com>
2025-05-14 10:43:54 -0300

    Create Main.java

---
+++ Clientes_Contas_Heranca/Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+Exercício Herança - Cliente / Contas
+
+Implemente a modelagem apresentada abaixo. O diagrama representa o modelo básico do projeto. Faça as adaptações necessárias. 
+
+Um Cliente pode possuir varias contas.
+A conta pode ser do tipo conta corrente ou conta poupança.
+No método MostrarSaldos é exibida todas as contas do cliente e os saldos de cada uma delas.
+As operações de saque possuem uma taxa a ser cobrada a cada operação, para conta corrente a taxa é de 5 reais, para poupança a taxa é de 15 reais.
+O depósito é feito de forma semelhante nas duas.
+Conta possui o método AplicarRendimento. Para conta corrente o rendimento é de 1,2% e para conta poupança será de 0,5%.
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+    
+	}
+}
